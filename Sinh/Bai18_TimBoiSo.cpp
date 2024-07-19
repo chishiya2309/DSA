4555cd6d6280864d3435baaf8f39ba860fc9b4a7
Hung Hao Han <lequanghung.work@gmail.com>
2024-07-19 22:02:05 +0700

    Update: Tim Boi So

--- Sinh/Bai18_TimBoiSo.cpp
+++ Sinh/Bai18_TimBoiSo.cpp
@@ -1,68 +1,69 @@
-#include <bits/stdc++.h>
+/*
+Một số được coi là số đẹp nếu nó chỉ bao gồm 2 số là 8 và 0. Bạn được cấp một số nguyên dương X, nhiệm vụ của bạn là tìm bội dương nhỏ nhất của X thỏa mãn bội này là số đẹp.
 
+Gợi ý : Sinh ra xâu nhị phân gồm 2 chữ số 0 và 8 với độ dài đủ lớn, đủ để có hết các bội tới số 300. Sau đó chuyển chuỗi thành số
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
 using namespace std;
-int n, a[20];
+string s;
+vector<ll> v;
 bool final = false;
 void khoitao()
 {
-    for(int i = 1; i <= n; i++)
+    for(int i = 0; i < s.size(); i++)
     {
-        a[i] = 0;
+        s[i] = '0';
     }
 }
 
 void sinh()
 {
-    int i = n;
-    while(i >= 0 && a[i] == 1)
+    int i = s.size() - 1;
+    while(i >= 0 && s[i] == '8')
     {
-        a[i] = 0;
+        s[i] = '0';
         i--;
     }
-    if(i == 0)
+    if(i == -1)
     {
         final = true;
     }else {
-        a[i] = 1;
+        s[i] = '8';
     }
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin >> n;
-    int s;
-    cin >> s;
-    khoitao();
-    int w[n + 3], v[n + 3];
-    int trongLuong = 0, giaTri = 0;
-    for(int i = 1; i <= n; i++)
-    {
-        cin >> w[i];
-    }
-    for(int i = 1; i <= n; i++)
-    {
-        cin >> v[i];
-    }
-    int tongGiaTri = -1e9;
+    s = string(19, '0');
+    s[s.size() - 1] = '8';
     while(!final)
     {
-        for(int i = 1; i <= n; i++)
-        {
-            if(a[i] == 1)
-            {
-                trongLuong += w[i];
-                giaTri += v[i];
-            }
-        }
-
-        if(trongLuong <= s)
-        {
-            tongGiaTri = max(tongGiaTri, giaTri);
-        }
-        trongLuong = 0;
-        giaTri = 0;
+        v.push_back(stoll(s));
         sinh();
     }
-    cout << tongGiaTri;
+    ll ans[303];
+    for(int i = 1; i <= 300; i++)
+    {
+        for(ll x : v)
+        {
+            if(x % i == 0)
+            {
+                ans[i] = x;
+                break;
+            }
+        }
+    }
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << ans[n] << endl;
+    }
     return 0;
 }
