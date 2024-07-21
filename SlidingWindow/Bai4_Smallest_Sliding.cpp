f46f5e238c13661192a5496dc89337e465d030a5
Hung Hao Han <lequanghung.work@gmail.com>
2024-07-21 16:04:18 +0700

    Update: Smallest Sliding

--- SlidingWindow/Bai4_Smallest_Sliding.cpp
+++ SlidingWindow/Bai4_Smallest_Sliding.cpp
@@ -1,3 +1,26 @@
+/*
+Cho xâu S chỉ gồm chữ cái in thường, nhiệm vụ của bạn là tìm chiều dài của xâu con liên tiếp nhỏ nhất có chứa đầy đủ các kí tự khác nhau của S.
+
+Ví dụ S = abcaaaabcad thì xâu con bcad có độ dài nhỏ nhất và chứa đầy đủ các kí tự khác nhau của S.
+
+Đầu vào
+Dòng duy nhất chứa xâu S
+
+Giới hạn
+1<=len(S)<=10^6
+
+Đầu ra
+In ra độ dài xâu con nhỏ nhất tìm được
+
+Ví dụ :
+Input 01
+Copy
+28teeeeeechhhh
+Output 01
+Copy
+11
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,30 +30,27 @@ int main() {
     cin.tie(NULL);
     string s;
     cin >> s;
-
-    unordered_map<char, int> char_count;
-
-    for(char c : s) {
+    map<char, int> char_count;
+    for(char c : s)
+    {
         char_count[c] = 0;
     }
     int distinct_chars = char_count.size();
 
     int start = 0, min_length = s.length();
-    int count = 0;
-
-    for(int end = 0; end < s.length(); end++) {
+    int cnt = 0;
+    for(int end = 0; end < s.size(); end++)
+    {
         if(++char_count[s[end]] == 1)
         {
-            count++;
+            cnt++;
         }
-
-        while(count == distinct_chars)
+        while(cnt == distinct_chars)
         {
             min_length = min(min_length, end - start + 1);
-
             if(--char_count[s[start]] == 0)
             {
-                count--;
+                cnt--;
             }
             start++;
         }
