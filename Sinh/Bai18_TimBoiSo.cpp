bdb29e5800006369342f3e720d2cf2480a4cb847
Hung Hao Han <lequanghung.work@gmail.com>
2024-07-22 20:30:18 +0700

    Update: Tim Boi So

--- Sinh/Bai18_TimBoiSo.cpp
+++ Sinh/Bai18_TimBoiSo.cpp
@@ -1,7 +1,39 @@
 /*
 Một số được coi là số đẹp nếu nó chỉ bao gồm 2 số là 8 và 0. Bạn được cấp một số nguyên dương X, nhiệm vụ của bạn là tìm bội dương nhỏ nhất của X thỏa mãn bội này là số đẹp.
 
-Gợi ý : Sinh ra xâu nhị phân gồm 2 chữ số 0 và 8 với độ dài đủ lớn, đủ để có hết các bội tới số 300. Sau đó chuyển chuỗi thành số
+Gợi ý : Sinh ra xâu nhị phân gồm 2 chữ số 0 và 8 với độ dài đủ lớn, đủ để có hết các bội tới số 300. Sau đó chuyển chuỗi thành số.
+
+Đầu vào
+Dòng đầu tiên là số bộ test T.
+
+T dòng tiếp theo mỗi dòng là một số nguyên dương N.
+
+Giới hạn
+1<=T<=300
+
+1<=N<=300
+
+Đầu ra
+Mỗi test in kết quả trên 1 dòng
+
+Ví dụ :
+Input 01
+Copy
+6
+240
+19
+55
+47
+287
+247
+Output 01
+Copy
+8880
+88008
+880
+80088
+8088808
+8000000008
 */
 
 #include <bits/stdc++.h>
@@ -9,20 +41,11 @@ Gợi ý : Sinh ra xâu nhị phân gồm 2 chữ số 0 và 8 với độ dài 
 #define endl '\n'
 using namespace std;
 string s;
-vector<ll> v;
 bool final = false;
-void khoitao()
-{
-    for(int i = 0; i < s.size(); i++)
-    {
-        s[i] = '0';
-    }
-}
-
 void sinh()
 {
     int i = s.size() - 1;
-    while(i >= 0 && s[i] == '8')
+    while (i >= 0 && s[i] == '8')
     {
         s[i] = '0';
         i--;
@@ -30,7 +53,8 @@ void sinh()
     if(i == -1)
     {
         final = true;
-    }else {
+    }else 
+    {
         s[i] = '8';
     }
 }
@@ -39,31 +63,32 @@ int main() {
     cin.tie(NULL);
     s = string(19, '0');
     s[s.size() - 1] = '8';
+    vector<ll> v;
     while(!final)
     {
         v.push_back(stoll(s));
         sinh();
     }
-    ll ans[303];
+    sort(v.begin(), v.end());
+    ll res[303];
     for(int i = 1; i <= 300; i++)
     {
         for(ll x : v)
         {
             if(x % i == 0)
             {
-                ans[i] = x;
+                res[i] = x;
                 break;
             }
         }
     }
-
     int t;
     cin >> t;
     while(t--)
     {
         int n;
         cin >> n;
-        cout << ans[n] << endl;
+        cout << res[n] << endl;
     }
     return 0;
 }
