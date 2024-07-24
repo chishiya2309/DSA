184b91a4fd7c47168edacbb7aac3463190565dc1
Hung Hao Han <lequanghung.work@gmail.com>
2024-07-24 20:23:08 +0700

    Update: Liet Ke Xau Ki Tu

--- Sinh/Bai24_LietKeXauKiTu.cpp
+++ Sinh/Bai24_LietKeXauKiTu.cpp
@@ -4,37 +4,57 @@ Cho chữ cái c in hoa và số nguyên K
 Hãy tìm cách liệt kê tất cả các xâu có K ký tự khác nhau được tạo ra bởi các chữ cái tính từ "A" đến ký tự c.
 
 Các ký tự trong xâu kết quả có thể được lặp lại, xem ví dụ mẫu để rõ hơn.
+
+Đầu vào
+Chữ cái c và K
+
+Giới hạn
+'A' < c <='G'
+
+0 < K < (c – 'A')
+
+Đầu ra
+Ghi ra lần lượt các xâu ký tự kết quả theo thứ tự từ điển trên từng dòng.
+
+Ví dụ :
+Input 01
+Copy
+C 2
+Output 01
+Copy
+AA
+AB
+AC
+BA
+BB
+BC
+CA
+CB
+CC
 */
 
 #include <bits/stdc++.h>
-
+#define endl '\n'
 using namespace std;
 char c;
 int k;
-char Xau[10];
+string s;
 bool final = false;
-void khoitao()
-{
-    for(int i = 1; i <= k; i++)
-    {
-        Xau[i] = 'A';
-    }
-}
 void sinh()
 {
-    int i = k;
-    while(i >= 1 && Xau[i] == c)
+    int i = k - 1;
+    while(i >= 0 && s[i] == c)
     {
         i--;
     }
-    if(i == 0)
+    if(i == -1)
     {
         final = true;
     }else {
-        Xau[i]++;
-        for(int j = i + 1; j <= k; j++)
+        s[i]++;
+        for(int j = i + 1; j < k; j++)
         {
-            Xau[j] = 'A';
+            s[j] = 'A';
         }
     }
 }
@@ -42,14 +62,10 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> c >> k;
-    khoitao();
+    s = string(k, 'A');
     while(!final)
     {
-        for(int i = 1; i <= k; i++)
-        {
-            cout << Xau[i];
-        }
-        cout << endl;
+        cout << s << endl;
         sinh();
     }
     return 0;
