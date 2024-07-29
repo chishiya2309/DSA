d7dbff25b58e55055165a309cd7b29ac1d07fedd
Hung Hao Han <lequanghung.work@gmail.com>
2024-07-29 17:19:13 +0700

    Update: Day Con Co Tong Le

--- QuayLui_NhanhCan/Bai_19_DayConCoTongLe.cpp
+++ QuayLui_NhanhCan/Bai_19_DayConCoTongLe.cpp
@@ -1,34 +1,3 @@
-/*
-Cho mảng A[] gồm N phần tử, liệt kê các tập con (giữ đúng thứ tự trước sau) của mảng A[] có tổng các phần tử là số lẻ, mỗi phần tử chỉ được lấy 1 lần. Chú ý nếu 2 tập hợp chứa 2 phần tử có giá trị giống nhau nhưng ở vị trí khác nhau thì được tính 2 tập hợp khác nhau.
-
-Đầu vào
-• Dòng 1 là N : số lượng phần tử trong mảng
-
-• Dòng 2 gồm N số trong mảng A[]
-
-Giới hạn
-• 2 <= N <= 15
-
-• 1 <= A[i] <= 1000
-
-Đầu ra
-• In ra các tập con thỏa mãn theo thứ tự từ điển tăng dần, nếu không tồn tại dãy con in ra NOT FOUND
-
-Ví dụ :
-Input 01
-4
-9 7 9 9
-Output 01
-7 
-7 9 9 
-9 
-9 
-9 
-9 7 9 
-9 7 9 
-9 9 9
-*/
-
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
