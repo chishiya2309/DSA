9f6f78eceb855d4f4445d077d72ba2ec813a47b1
Hung Hao Han <lequanghung.work@gmail.com>
2024-08-07 20:10:58 +0700

    Update: Day Con Co Tong Lon Nhat Dung Chia Va Tri

---
+++ ChiaVaTri/Bai_6_DayConCoTongLonNhat_ChiaVaTri.cpp
@@ -0,0 +1,77 @@
+/*
+Cho dãy số nguyên A[] gồm N phần tử, bạn hãy là tính tổng của dãy con liên tiếp có tổng lớn nhất.
+
+Đầu vào
+Dòng đầu tiên chứa số nguyên dương N
+
+Dòng thứ 2 chứa N số nguyên của dãy A[]
+
+Giới hạn
+1<=N<=10^5
+
+1<=abs(A[i])<=10^6
+
+Đầu ra
+In ra đáp án của bài toán
+
+Ví dụ :
+Input 01
+5
+-3 6 1 -3 6
+Output 01
+10
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ll crossing_sum(int a[], int l, int mid, int r)
+{
+    ll sum_left = 0, max_sum_left = 0;
+    ll sum_right = 0, max_sum_right = 0;
+    for(int i = mid; i >= l; i--)
+    {
+        sum_left += a[i];
+        max_sum_left = max(sum_left, max_sum_left);
+    }
+
+    for(int i = mid + 1; i <= r; i++)
+    {
+        sum_right += a[i];
+        max_sum_right = max(sum_right, max_sum_right);
+    }
+
+    return max_sum_left + max_sum_right;
+}
+
+ll xuly(int a[], int l, int r)
+{
+    if(l == r)
+    {
+        return a[l];
+    }
+    
+    int mid = (l + r) / 2;
+    return max({
+        xuly(a, l, mid),
+        xuly(a, mid + 1, r),
+        crossing_sum(a, l, mid, r)
+    });
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+
+    int a[n];
+    for(int &x : a)
+    {
+        cin >> x;
+    }
+
+    cout << xuly(a, 0, n - 1);
+    return 0;
+}
