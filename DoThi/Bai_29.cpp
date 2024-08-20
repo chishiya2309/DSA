9bcf5b05411830e90df5f1da0a2af92c603cbbcc
Hung Hao Han <lequanghung.work@gmail.com>
2024-08-20 09:10:04 +0700

    Update: Danh Sach Canh Sang Ma Tran Ke (Vo Huong)

---
+++ DoThi/Bai_29.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, s, t, u, v, a[1003][1003];
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+int bfs(pair<int, int> start, pair<int, int> end)
+{
+    queue<pair<pair<int, int>, int>>q;
+    q.push({start, 0});
+}
+
+int bfs()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+
+
+    return 0;
+}
