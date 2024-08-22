390dff9f914d31b66bdd2461f3b8ecb8771cda20
Hung Hao Han <lequanghung.work@gmail.com>
2024-08-22 20:50:16 +0700

    Update: Liet Ke Tap Con

--- Sinh/Bai13_LietKeTapCon.cpp
+++ Sinh/Bai13_LietKeTapCon.cpp
@@ -12,8 +12,10 @@ In ra các tập con của N theo thứ tự từ điển tăng dần.
 
 Ví dụ :
 Input 01
+Copy
 4
 Output 01
+Copy
 1 
 1 2 
 1 2 3 
@@ -34,9 +36,12 @@ Output 01
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
-int n, x[10];
+
+int n;
+int x[10];
 bool final = false;
-vector<vector<int>> ans;
+vector<vector<int>> v;
+
 void init()
 {
     for(int i = 1; i <= n; i++)
@@ -44,47 +49,56 @@ void init()
         x[i] = 0;
     }
 }
+
 void sinh()
 {
     int i = n;
-    while(i >= 1 && x[i] == 1)
+    while(i > 0 && x[i] == 1)
     {
         x[i] = 0;
         i--;
     }
+
     if(i == 0)
     {
         final = true;
-    }else {
+    }else
+    {
         x[i] = 1;
     }
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
     cin >> n;
     init();
+    
     while(!final)
     {
         vector<int> tmp;
         for(int i = 1; i <= n; i++)
         {
-            if(x[i] == 1)
+            if(x[i])
             {
                 tmp.push_back(i);
             }
         }
-        ans.push_back(tmp);
+        v.push_back(tmp);
         sinh();
     }
-    sort(ans.begin(), ans.end());
-    for(auto v : ans)
+
+    sort(v.begin(), v.end());
+
+    for(auto it : v)
     {
-        for(int num : v)
+        for(int num : it)
         {
             cout << num << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
