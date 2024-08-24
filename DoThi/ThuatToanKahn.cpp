ebd67951d486a7d64aaf6208f3f96b3120cd6d80
Hung Hao Han <lequanghung.work@gmail.com>
2024-08-24 18:23:52 +0700

    Update: Thuat Toan Kahn

---
+++ DoThi/ThuatToanKahn.cpp
@@ -0,0 +1,61 @@
+/*
+1 kiểu sắp xếp topo dùng bfs, nếu duyệt hết được tất cả các đỉnh thì đồ thị không có chu trình
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> ke[1003];
+int bac[1003];
+int visited[1003];
+
+void kahn()
+{
+    queue<int> q;
+    for(int i = 1; i <= n; i++)
+    {
+        if(bac[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+    int dem = 0;
+    while(!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        dem++;
+        for(int j : ke[u])
+        {
+            bac[j]--;
+            if(bac[j] == 0)
+            {
+                q.push(j);
+            }
+        }
+    }
+
+    //Nếu duyệt đủ đỉnh thì không có chu trình, ngược lại thì có chu trình
+    if(dem == n)
+    {
+        cout << 0;
+    }else
+    {
+        cout << 1;
+    }
+}
+
+signed main()
+{
+    cin >> n >> m;
+    for(int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        ke[x].push_back(y);
+        bac[y]++;
+    }
+
+    kahn();
+}
