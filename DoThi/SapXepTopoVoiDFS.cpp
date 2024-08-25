5e95b3f3eb40b22378b084db3c35067c607ac078
Hung Hao Han <lequanghung.work@gmail.com>
2024-08-25 15:01:03 +0700

    Update: Sap Xep Topo Dung DFS

--- DoThi/SapXepTopoVoiDFS.cpp
+++ DoThi/SapXepTopoVoiDFS.cpp
@@ -1,22 +1,23 @@
 /*
 Cơ chế hoạt động: Đỉnh nào không đi được nửa thì đẩy vào vector rồi lật ngược, cơ chế giống ngăn xếp, xếp chồng lên nhau rồi lấy ra từ trên đến hết
 */
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int n, m;
-vector<int> ke[1003];
+set<int> ke[1003];
 vector<int> topo;
 bool visited[1003];
 
-void dfs(int u)
+void sapXepTopo(int u)
 {
     visited[u] = 1;
     for(int j : ke[u])
     {
         if(!visited[j])
         {
-            dfs(j);
+            sapXepTopo(j);
         }
     }
     topo.push_back(u);
@@ -29,14 +30,14 @@ signed main()
     {
         int x, y;
         cin >> x >> y;
-        ke[x].push_back(y);
+        ke[x].insert(y);
     }
 
     for(int i = 1; i <= n; i++)
     {
         if(!visited[i])
         {
-            dfs(i);
+            sapXepTopo(i);
         }
     }
 
